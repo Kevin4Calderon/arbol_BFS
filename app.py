670beb915f7bd63d24b336616eb2553913240e09
Kevin4Calderon <kevinyoan4@gmail.com>
2026-03-19 23:05:43 -0600

    Proyecto BFS listo para deploy

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from BFS import buscar_solucion_BFS
+
+app = Flask(__name__)
+
+@app.route("/")
+def resolver():
+    # Valores por defecto
+    inicial = request.args.get("inicial", "4,2,3,1")
+    objetivo = request.args.get("objetivo", "1,2,3,4")
+
+    # Convertir a listas
+    estado_inicial = list(map(int, inicial.split(",")))
+    solucion = list(map(int, objetivo.split(",")))
+
+    # Ejecutar BFS
+    nodo_solucion = buscar_solucion_BFS(estado_inicial, solucion)
+
+    resultado = []
+    nodo = nodo_solucion
+
+    while nodo.get_padre() != None:
+        resultado.append(nodo.get_datos())
+        nodo = nodo.get_padre()
+
+    resultado.append(estado_inicial)
+    resultado.reverse()
+
+    return str(resultado)
+
+if __name__ == "__main__":
+    app.run()
