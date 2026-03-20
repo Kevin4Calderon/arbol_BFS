e7245cc3b290a40cad4d955b1d932e27c39f7f88
Kevin4Calderon <kevinyoan4@gmail.com>
2026-03-20 00:02:13 -0600

    Fix puerto para Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, send_file
 from BFS import buscar_solucion_BFS
+import os
 
 app = Flask(__name__)
 
@@ -34,4 +35,5 @@ def resolver():
     return str(resultado)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
